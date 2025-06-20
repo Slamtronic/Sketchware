75b9077ad0b498d51c83fc05c481d92b43e70918
Slamtronic <139569102+Slamtronic@users.noreply.github.com>
2025-06-20 21:39:14 +0100

    Add files via upload

---
+++ File_store.java
@@ -0,0 +1,14 @@
+package slam.tronic;
+
+public class File_store{
+    
+    String str;
+    public File_store(){}
+    
+    public File_store(String s)
+    {  str=s;            }
+    
+    public String getstr()
+    { return str;}
+    
+}
